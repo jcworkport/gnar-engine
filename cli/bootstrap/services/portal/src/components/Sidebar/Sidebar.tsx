161f6914554b830786aa9f5ae1be110f618b3fa5
Adam Kent <adam@gnar.co.uk>
2025-11-18 22:24:53 +0000

    portal layout

---
+++ cli/bootstrap/services/portal/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,17 @@
+
+
+function Sidebar() {
+
+    return (
+        <div className="sidebar">
+            <ul>
+                <li>Dashboard</li>
+                <li>Settings</li>
+                <li>Profile</li>
+                <li>Logout</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
